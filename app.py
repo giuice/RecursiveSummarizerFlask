aa2000b637deaac959b19d58fe1d6342cd90b0df
Giuliano Lemes <giuice@gmail.com>
2023-03-28 22:39:39 -0300

    Functional with time and summarization

--- app.py
+++ app.py
@@ -30,30 +30,20 @@ def get_transcript(video_url):
     transcript = YouTubeTranscriptApi.get_transcript(_id)
     return transcript
 
-API_KEY = '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'
+API_KEY = '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'
 
 def summarize_transcript(transcript, language):
-    summarized_transcript = []
-    text = ""
-    token_count = 0
-    start_time = 0
-    error_message = None
-    
 
-    #flash(transcript)
     # Compute the duration of each 5 minute interval
     interval_duration = 5 * 60  # 5 minutes in seconds
     num_intervals = math.ceil(transcript[-1]['start'] / interval_duration)  # Round up to the nearest interval
-    #flash(f"num_intervals: {transcript[-1]['start']}")
     interval_durations = [interval_duration] * num_intervals
-    #flash(f"Interval durations: {interval_durations}")
+
     interval_durations[-1] = transcript[-1]['start'] % interval_duration
-    #flash(f"Interval durations2: {interval_durations}")
     # Extract the text property from each 5 minute interval
     text_by_interval = []
     start_time1 = 0
     for duration in interval_durations:
-        #flash(f"duration: {duration}")
         end_time1 = start_time1 + duration
         
         text1 = [d['text'] for d in transcript if start_time1 <= d['start'] < end_time1]
@@ -61,36 +51,11 @@ def summarize_transcript(transcript, language):
         text1 = summarize_text_turbo(text1, language=language)
 
         obj = {'start_time': "{:.2f}".format(start_time1/60), 'end_time': "{:.2f}".format(end_time1/60), 'text': text1[0].replace('\n', "<br />")}
-        #obj = {'start_time': "{:.2f}".format(start_time1/60), 'end_time': "{:.2f}".format(end_time1/60), 'text': text1.replace('\n', "<br />")}
+
         text_by_interval.append(obj)
         start_time1 = end_time1
-    #flash(f"text_by_interval: {text_by_interval}")
+
     return  text_by_interval, None
-    for item in transcript:
-        text += item['text'] + ' '
-        token_count += len(item['text'].split())
-
-        if token_count >= 1900:
-            summary, error = summarize_text(text, language)
-            if error:
-                error_message = error
-                break
-            end_time = item['start']
-            summarized_transcript.append({'title': f"Summary {start_time:.2f}-{end_time:.2f}", 'summary': summary})
-            text = ""
-            token_count = 0
-            start_time = item['start'] + item['duration']
-
-    if text and not error_message:
-        summary, error = summarize_text(text, language)
-        if error:
-            error_message = error
-        else:
-            end_time = transcript[-1]['start'] + transcript[-1]['duration']
-            summarized_transcript.append({'title': f"Summary {start_time:.2f}-{end_time:.2f}", 'summary': summary})
-
-    return summarized_transcript, error_message
-
 
 def summarize_text(text, language):
     try:
@@ -124,11 +89,11 @@ def summarize_text(text, language):
     
 def summarize_text_turbo(text, model="gpt-3.5-turbo", temp=0,  language="English"):
     try:
-        prompt = """Write a summarize version of the following text
+        prompt = """Write a summarize version of the following [text]
         If possible make a title
-        Always output in modern HTML format with headers, bullets.
-        Write the text in <<LANG>>
-        The text is : <<SUMMARY>>"""
+        Always format the output [text] in modern HTML with headers, bullets.
+        Write the [text] in <<LANG>>
+        The [text] is : <<SUMMARY>>"""
 
         prompt = prompt.replace('<<LANG>>', language)
         prompt = prompt.replace('<<SUMMARY>>', text)
